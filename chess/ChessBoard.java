01a866d3b4b66c3a633e66690bb5cf7b3696755e
CHE-Lijia <33571952+CHE-Lijia@users.noreply.github.com>
2017-11-20 12:50:36 +0800

    Add files via upload

--- chess/ChessBoard.java
+++ chess/ChessBoard.java
@@ -2,6 +2,7 @@ package chess;
 
 import piece.Piece;
 import run.Check;
+import run.Command;
 
 public class ChessBoard 
 {
@@ -9,6 +10,7 @@ public class ChessBoard
 	private static ChessBoard theChessBoard = new ChessBoard();
 	private static Check checkBlack = new Check(true);
 	private static Check checkWhite = new Check(false);
+	private static boolean turn = true;
 	
 	public ChessBoard()
 	{
@@ -28,4 +30,24 @@ public class ChessBoard
 	public Cell getFromPosition(int x,int y){
 		return board[x][y];
 	}
+	
+	
+	private boolean inBoundary(int x,int y,int target_x,int target_y){
+		if(x>7 || y>7 || x<0 || y<0){
+			System.out.print(" Position out of board.");
+			return false;
+		}
+		if(target_x >7 || target_y >7 || target_x <0 || target_y <0){
+			System.out.print(" Target position out of board.");
+			return false;
+		}
+		return true;
+		
+	}
+	
+	public void changeTurn(){
+		turn = !turn;
+	}
+	
+	
 }
